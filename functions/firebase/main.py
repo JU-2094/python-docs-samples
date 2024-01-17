8f40f326e736d68f74ea9ffb199c2c0673a4f4cc
Josue Urbina <jfurbina@google.com>
2024-01-17 00:43:29 +0000

    Test

--- functions/firebase/main.py
+++ functions/firebase/main.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# VSCODE TEST for further development
 # [START functions_firebase_analytics]
 from datetime import datetime
 
